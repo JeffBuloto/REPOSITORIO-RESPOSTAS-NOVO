6ad97209fdca54ed1eea17e6f7c3ddf217da52ea
JeffBuloto <114021308+JeffBuloto@users.noreply.github.com>
2025-08-20 11:45:28 -0300

    Add files via upload
    Resolução do desafio 1- cálculo de média de notas de alunos.

---
+++ Desafio 1.py
@@ -0,0 +1,26 @@
+A1 = str(input('Escreva seu nome:      '))
+N1 = int(input('Digite a primeira nota:  '))
+N2 = int(input('Digite a segunda  nota:  '))
+N3 = int(input('Digite a terceira nota:  '))
+N4 = int(input('Digite a quarta nota:    '))
+
+A2 = str(input('Escreva seu nome:      '))
+N5 = int(input('Digite a primeira nota:  '))
+N6 = int(input('Digite a segunda  nota:  '))
+N7 = int(input('Digite a terceira nota:  '))
+N8 = int(input('Digite a quarta nota:    '))
+
+A3 = str(input('Escreva seu nome:      '))
+N9 = int(input('Digite a primeira nota:   '))
+N10 = int(input('Digite a segunda  nota:  '))
+N11 = int(input('Digite a terceira nota:  '))
+N12 = int(input('Digite a quarta nota:    '))
+
+#====================================================#
+
+print("Aluno", A1,  "Média: ", (N1 + N2 + N3 + N4) / 4)
+
+print("Aluno", A2,  "Média: ", (N5 + N6 + N7 + N8) / 4)
+
+print("Aluno ", A3,  "Média: ",  (N9 + N10 + N11 + N12) / 4)
+ 
